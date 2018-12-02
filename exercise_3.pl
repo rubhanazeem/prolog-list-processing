4d656582aec0003f33c422138ce691ecea3d1b52
rubhanazeem <rubhanazeem@gmail.com>
2018-12-02 16:17:09 +0100

    exercise 3 solution updated

--- exercise_3.pl
+++ exercise_3.pl
@@ -10,3 +10,16 @@ directlyIn(natasha, irina).
 
 in(X,Y) :- directlyIn(X,Y).
 in(X,Y) :- directlyIn(X,Z), in(Z,Y).
+
+%% Exercise 3.3
+
+directTrain(saarbruecken,dudweiler).
+directTrain(forbach,saarbruecken).
+directTrain(freyming,forbach).
+directTrain(stAvold,freyming).
+directTrain(fahlquemont,stAvold).
+directTrain(metz,fahlquemont).
+directTrain(nancy,metz).
+
+travelFromTo(X, Y) :- directTrain(X, Y).
+travelFromTo(X, Y) :- directTrain(X, Z), travelFromTo(Z, Y).
