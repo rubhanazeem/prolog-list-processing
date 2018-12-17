9d6bf4adb3d116221ba0633c9c6eef0423852562
rubhanazeem <rubhanazeem@gmail.com>
2018-12-18 00:30:11 +0100

    adding small/2

--- list_processing.pl
+++ list_processing.pl
@@ -4,3 +4,11 @@ accMax([H|T], A, Max) :- A =< H, accMax(T, H, Max).
 accMax([H|T], A, Max) :- A > H, accMax(T, A, Max).
 
 max(List, X) :- List = [H|_], accMax(List, H, X).
+
+
+%% get smallest element of the list
+accSmall([H|T], A, Small) :- A > H, accSmall(T, H, Small).
+accSmall([H|T], A, Small) :- A =< H, accSmall(T, A, Small).
+accSmall([], A, A).
+
+small(List, X) :- List = [H|_], accSmall(List, H, X).
